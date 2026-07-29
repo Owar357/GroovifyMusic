661144643240774093c0216b54f58a12fa96a2d2
0war357 <bart89761@gmail.com>
2026-07-28 20:03:07 -0600

    feat: primer commit

---
+++ src/main/java/com/ITCHA2026/GroovyfyMusic/GroovyfyMusicApplication.java
@@ -0,0 +1,13 @@
+package com.ITCHA2026.GroovyfyMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyfyMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyfyMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITCHA2026/GroovyfyMusic/GroovyfyMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITCHA2026.GroovyfyMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovyfyMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
